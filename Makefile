c3d15c4af78e05055f237570310a2d63ebd09ee4
Melissa Andrade Garcia Da Silva <meandrad@c1r8p1.42sp.org.br>
2025-04-24 22:20:50 -0300

    aaaaaaaaaaaa

---
+++ Makefile
@@ -0,0 +1,30 @@
+NAME = fractol
+CC = cc
+CFLAGS = -g -I/minilibx -Wall -Wextra -Werror
+MLXFLAGS = -L./minilibx-linux -lmlx -lXext -lX11 -lm
+
+
+SRC = \
+	src/handlers.c \
+	src/init.c \
+	src/lib_utils.c \
+	src/main.c \
+	src/math.c \
+	src/render.c \
+
+OBJ = $(SRC:%.c=%.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+		$(CC) $(CFLAGS) $(OBJ) $(MLXFLAGS) -o $(NAME)
+
+clean:
+		rm -f $(OBJ)
+
+fclean: clean
+		rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ src/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 21:16:53 by meandrad          #+#    #+#             */
+/*   Updated: 2025/04/24 21:47:17 by meandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/fractol.h"
+
+int	main(int argc, char *argv[])
+{
+	t_fractal	fractal;
+
+	if ((argc == 2 && !ft_strncmp(argv[1], "mandelbrot", 10))
+		|| (argc == 4 && !ft_strncmp(argv[1], "julia", 5)))
+	{
+		fractal.name = argv[1];
+		if (!ft_strncmp(fractal.name, "julia", 5))
+		{
+			fractal.julia_x = atodbl(argv[2]);
+			fractal.julia_y = atodbl(argv[3]);
+		}
+		fractal_init(&fractal);
+		fractal_render(&fractal);
+		mlx_loop(fractal.mlx);
+	}
+	else
+	{
+		ft_putstr(ERROR, 2);
+		exit(EXIT_FAILURE);
+	}
+}
